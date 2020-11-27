8cb7ef549aa00c09ee5da0c1e542c6794852a9b0
ShubhamKulkarni14 <shubhuk14@gmail.com>
2020-11-27 23:08:17 +0530

    Added :Checking Part Time Employee Wages

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,20 +1,29 @@
 class EmpWageComputation
 {
-        public static final int WAGE_PER_HR=20,HALF_DAY=1,FULL_DAY=2;
-        public static void main(String args[])
-        {
-                int random=(int)Math.floor(Math.random()*10)%3;
-                int workingHr=0,salary=0;
-                if(random == FULL_DAY)
+    public static final int WAGE_PER_HR=20,PART_TIME=1,FULL_DAY=2;
+    public static void main(String args[])
+{
+
+        int random=(int)Math.floor(Math.random()*10)%3;
+        int workingHr=0,salary=0;
+
+        if(random == FULL_DAY)
                 {
-                        System.out.println("Employee is PRESENT for Full Day");
+                        System.out.println("EMPLOYEE PRESENT FOR FULL DAY");
                         workingHr=8;
+                }
+
+         if(random == PART_TIME)
+                {
+                        System.out.println("EMPLOYEE PRESENT FOR HALF DAY");
+                        workingHr=4;
 		}
-                else
+         else
 		{
-                        System.out.println("employee is absent");
-		}
+                        System.out.println("EMPLOYEE IS ABSENT");
+                }
+
                 salary=WAGE_PER_HR*workingHr;
-                System.out.println("Employee Wage: "+salary);
-        }
+                System.out.println("EMPLOYEE WAGE : "+salary);
+    }
 }
